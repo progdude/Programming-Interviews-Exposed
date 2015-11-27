5385175ce4a53b73784f4fa5ef7fe425aee564e2
archit rathi <architrathi10@gmail.com>
2015-11-27 14:56:14 -0500

    did second linked list problem

--- linked_lists/LinkedList.java
+++ linked_lists/LinkedList.java
@@ -24,10 +24,62 @@ public class LinkedList {
 	
 	public void print(){
 		Node temp = head;
-		while(temp.next!=null){
-			System.out.println(temp.data);
+		while(temp!=null){
+			System.out.print(temp.data+" ");
 			temp = temp.next;
 		}
+		System.out.println();
+	}
+	
+	public boolean remove(int x){
+		Node temp = head;
+		
+		if(head.data == x){
+			head = head.next;
+			temp.next = null;
+			return true;
+		}
+		
+		while(temp!=null){
+			if(temp.next.data == x){
+				temp.next = temp.next.next;
+				return true;
+			}
+			temp = temp.next;
+		}
+		return false;
+	}
+	
+	public void insert(int x){
+		Node temp = head;
+		
+		if(head == null){
+			head = new Node(x);
+			return;
+		}
+	
+		while(temp.next!=null){
+			temp=temp.next;
+		}
+		
+		temp.next = new Node(x);
+		
+	}
+	
+	public boolean insertAfter(int x, int y){
+		Node temp = head;
+		
+		while(temp!=null){
+			if(temp.data == x){
+				Node t = temp.next;
+				temp.next = new Node(y);
+				temp.next.next = t;
+				return true;
+			}
+			temp=  temp.next;
+		}
+		return false;
+		
 	}
 
 	
--- linked_lists/Testing.java
+++ linked_lists/Testing.java
@@ -5,8 +5,33 @@ public class Testing {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		/*Stacks*/
-
+		/*Stacks
+		Stack s = new Stack();
+		s.push(5);
+		s.push(7);
+		s.push(10);
+		s.push(12);
+		System.out.print("Inital Print: ");s.print();
+		s.pop();
+		s.pop();
+		System.out.print("Print after two pops: ");s.print();
+		s.pop();
+		System.out.print("Print after three pops: ");s.print();
+		End Stacks*/
+		
+	 /* Insert After and Remove*/
+		LinkedList li = new LinkedList();
+		li.insert(6);
+		li.insert(8);
+		li.insert(200);
+		li.insert(9);
+		li.print();
+		li.insertAfter(6,7);
+		li.print();
+		li.remove(200);
+		li.print();
+		
+		
 	}
 
 }
